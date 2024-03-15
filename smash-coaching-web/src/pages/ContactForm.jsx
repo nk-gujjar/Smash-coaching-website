d8d464a6b6fa30255fb33b9d6d26efcd941bc656
Nitesh kumar <niteshgurjar2612@gmail.com>
2024-03-15 14:24:43 +0530

    connect contact form to backend

--- smash-coaching-web/src/pages/ContactForm.jsx
+++ smash-coaching-web/src/pages/ContactForm.jsx
@@ -1,20 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push } from "firebase/database"; // Correct import for Realtime Database usage
 import './Welcomepage.css';
+import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from '../Admin/Firebasekeys';
+
+// Initialize Firebase (Replace with your actual Firebase config)
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAFaFFOSUqKaIJLfpxyEd0oU9jJRQmAxqY",
+//   authDomain: "smash-coaching-website.firebaseapp.com",
+//   projectId: "smash-coaching-website",
+//   storageBucket: "smash-coaching-website.appspot.com",
+//   messagingSenderId: "532639568873",
+//   appId: "1:532639568873:web:bddd5527dd99911f80b178",
+//   measurementId: "G-MVKTS30KYF"
+// };
+
+// Initialize Firebase app and Realtime Database instance
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app); // Get a reference to the database
 
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Sending data to Firebase Realtime Database
+    push(ref(database, 'contacts'), formData)
+      .then(() => {
+        console.log('Data sent successfully!');
+        // You can perform further actions here like showing a success message, resetting the form, etc.
+      })
+      .catch((error) => {
+        console.error('Error sending data: ', error);
+      });
+  };
+
   return (
     <div className="form">
       <h1 className="main-headings center">
         <span className="main-span">Contact Us</span>
       </h1>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="text" placeholder="write your message..." />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleChange} />
+        <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} />
+        <input type="text" name="message" placeholder="Write your message..." value={formData.message} onChange={handleChange} />
         <button type="submit" className="form-btn">Submit</button>
       </form>
     </div>
   );
-}
+};
 
 export default ContactForm;
